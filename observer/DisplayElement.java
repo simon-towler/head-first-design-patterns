e3b053dd32edcc7285e2645f34d787fb1eca9dee
simon-towler <simon.towler@icloud.com>
2019-12-30 13:33:44 +0100

    Bug fix: unclosed comment in DisplayElement interface.

--- observer/DisplayElement.java
+++ observer/DisplayElement.java
@@ -2,5 +2,6 @@ public interface DisplayElement {
   /*
    * The DisplayElement interface includes just one method, display(), that we
    * will call when the display element needs to be displayed
+   */
   public void display();
 }
