3d79a1107ef544f1857e9bc0aefafc7087c2c9ac
simon-towler <simon.towler@icloud.com>
2020-03-21 14:51:11 +0100

    Add Clam and Veggie pizza, and remove Greek.

--- factory/PizzaStore.java
+++ factory/PizzaStore.java
@@ -8,10 +8,12 @@ Pizza orderPizza(String type) {
 */
   if(type.equals("cheese")){
     pizza = new CheesePizza();
-  } else if (type.equals("greek")) {
-    pizza = new GreekPizza();
+  } else if (type.equals("clam")) {
+    pizza = new ClamPizza();
   } else if (type.equals("pepperoni")){
     pizza = new PepperoniPizza();
+  } else if (type.equals("veggie")){
+    pizza = new VeggiePizza();
   }//TODO check for unrecognised type
 
   pizza.prepare();
