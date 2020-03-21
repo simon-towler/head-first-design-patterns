7843eaa31ee7bad8ff438a6974ca87771811cf11
simon-towler <simon.towler@icloud.com>
2020-03-21 14:38:12 +0100

    Handle different types of pizza.

--- factory/PizzaStore.java
+++ factory/PizzaStore.java
@@ -1,5 +1,18 @@
-Pizza orderPizza() {
-  Pizza pizza = new Pizza();
+Pizza orderPizza(String type) {
+  Pizza pizza;
+
+/*
+ * Based on the type of pizza, we instantiate the appropriate concrete class
+ * and assign it to the pizza instance variable.
+ * Note that eah pizza has to implement the Pizza interface
+*/
+  if(type.equals("cheese")){
+    pizza = new CheesePizza();
+  } else if (type.equals("greek")) {
+    pizza = new GreekPizza();
+  } else if (type.equals("pepperoni")){
+    pizza = new PepperoniPizza();
+  }//TODO check for unrecognised type
 
   pizza.prepare();
   pizz.bake();
