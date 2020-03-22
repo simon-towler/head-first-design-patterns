7bd51faa402f96c15cc841a70836a49275bf2f91
simon-towler <simon.towler@icloud.com>
2020-03-22 10:40:48 +0100

    Produces expected output.

--- factory/PizzaRunner.java
+++ factory/PizzaRunner.java
@@ -1,7 +1,8 @@
 public class PizzaRunner {
 
   public static void main (String[] args) {
-    PizzaStore pizzaStore = new PizzaStore();
+    SimplePizzaFactory factory = new SimplePizzaFactory();
+    PizzaStore pizzaStore = new PizzaStore(factory);
     pizzaStore.orderPizza("veggie");
   }
 
