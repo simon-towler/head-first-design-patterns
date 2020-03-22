f1c0175ab013f240adbfb0d19880f12ee46fb24e
simon-towler <simon.towler@icloud.com>
2020-03-22 13:07:27 +0100

    Added a Pizza base class (no pun intended)

---
+++ factory/Pizza.java
@@ -0,0 +1,14 @@
+public class Pizza {
+  public void prepare() {
+    System.out.println("This pizza prepared.");
+  }
+  public void bake() {
+    System.out.println("This pizza baked.");
+  }
+  public void cut() {
+    System.out.println("This pizza cut.");
+  }
+  public void box() {
+    System.out.println("This pizza boxed.");
+  }
+}
